a1831b6a36bc1953b2ae89d238158b6d31bb16fd
Virginia Wu <virginiawu11@gmail.com>
2021-11-22 21:35:36 -0500

    add basic form with material-ui and formik

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 function App() {
+  const formik = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+    },
+    onSubmit: (values) => {
+      console.log(JSON.stringify(values));
+    },
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={formik.handleSubmit}>
+        <TextField
+          id="firstName"
+          name="firstName"
+          label="First Name"
+          margin="normal"
+          value={formik.values.firstName}
+          onChange={formik.handleChange}
+        />
+        <TextField
+          id="lastName"
+          name="lastName"
+          label="Last Name"
+          margin="normal"
+          value={formik.values.lastName}
+          onChange={formik.handleChange}
+        />
+        <TextField
+          id="email"
+          name="email"
+          label="Email Address"
+          margin="normal"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+        />
+        <Button variant="outlined">Submit</Button>
+      </form>
     </div>
   );
 }
